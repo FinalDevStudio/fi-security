522dbccfea0ccd6fb6188bfca19744a28bc9faf9
Santiago G. Marín <santiago.marin@dotstudio.io>
2019-12-31 14:08:05 -0300

    Improved implementation

--- test/index.js
+++ test/index.js
@@ -1,17 +1,16 @@
-'use strict';
-
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const base64url = require('base64url');
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const request = require('request');
 const express = require('express');
-const security = require('../lib');
 const crypto = require('crypto');
+
+const security = require('../lib');
 
 /* The Security component configuration */
 const config = {
-  debug: true,
+  debug: false,
 
   p3p: 'ABCDEF',
 
@@ -19,6 +18,17 @@ const config = {
     exclude: [
       {
         path: '/no-csrf'
+      },
+      {
+        path: ['/no-csrf', '/another']
+      },
+      {
+        path: ['/no-csrf', '/another'],
+        method: 'get'
+      },
+      {
+        path: ['/no-csrf', '/another'],
+        method: ['get', 'post']
       }
     ]
   },
@@ -43,33 +53,29 @@ const config = {
   nosniff: true
 };
 
-describe('Fi Security', function() {
+describe('Fi Security', function () {
   let $server, $http, $token;
 
-  before(function(done) {
+  before(async function () {
     /* Create the express app */
     const app = express();
 
     /* Body parser first */
-    app.use(
-      bodyParser.urlencoded({
-        extended: false
-      })
-    );
+    app.use(bodyParser.urlencoded({
+      extended: false
+    }));
 
     app.use(bodyParser.json());
 
     /* Initialize the session before anything else */
-    app.use(
-      session({
-        secret: base64url(crypto.randomBytes(48)),
-        saveUninitialized: true,
-        resave: true,
-        cookie: {
-          secure: false
-        }
-      })
-    );
+    app.use(session({
+      secret: base64url(crypto.randomBytes(16)),
+      saveUninitialized: true,
+      resave: true,
+      cookie: {
+        secure: false
+      }
+    }));
 
     /* Initialize the Security component before any route declaration */
     security(app, config);
@@ -89,37 +95,87 @@ describe('Fi Security', function() {
 
     /* Error handler */
     app.use((err, req, res, next) => {
-      // eslint-disable-line
-      console.log('\n');
-      console.error('   ', err);
-      console.log('');
-
       res.end();
     });
 
-    $server = app.listen(0, () => {
-      /* Initialize the request object */
-      $http = request.defaults({
-        baseUrl: 'http://localhost:' + $server.address().port,
-        jar: true
-      });
+    await new Promise(resolve => {
+      $server = app.listen(0, () => {
+        /* Initialize the request object */
+        $http = request.defaults({
+          baseUrl: `http://localhost:${$server.address().port}`,
+          jar: true
+        });
 
-      done();
+        resolve();
+      });
     });
   });
 
-  after(() => {
-    $server.close();
+  after(async function () {
+    if ($server) {
+      await $server.close();
+    }
   });
 
-  describe('object', function() {
-    it('should be a function', function() {
+  describe('object', function () {
+    it('should be a function', function () {
       expect(security).to.be.a('function');
     });
+
+    it('should initialize with debug as true', function () {
+      const app = express();
+      const cfg = {
+        ...config,
+        debug: true
+      };
+
+      expect(() => security(app, cfg)).to.not.throw();
+    });
+
+    it('should initialize with debug as a function', function () {
+      const app = express();
+      const cfg = {
+        ...config,
+        debug: msg => console.log(`[TEST] ${msg}`)
+      };
+
+      expect(() => security(app, cfg)).to.not.throw();
+    });
+
+    it('should not initialize with invalid method names', function () {
+      const app = express();
+      const cfg = {
+        ...config,
+        debug: false,
+        csrf: {
+          exclude: [{
+            method: 'lol',
+            path: '/'
+          }]
+        }
+      };
+
+      expect(() => security(app, cfg)).to.throw();
+    });
+
+    it('should not initialize with invalid exclude definition', function () {
+      const app = express();
+      const cfg = {
+        ...config,
+        debug: false,
+        csrf: {
+          exclude: [{
+            method: 'get'
+          }]
+        }
+      };
+
+      expect(() => security(app, cfg)).to.throw();
+    });
   });
 
-  describe('server', function() {
-    it('should respond a 200 status code and "Hello Word!" as body', function(done) {
+  describe('server', function () {
+    it('should respond a 200 status code and "Hello Word!" as body', function (done) {
       $http.get('/', (err, res, body) => {
         expect(err).to.be.null;
 
@@ -135,8 +191,8 @@ describe('Fi Security', function() {
     });
   });
 
-  describe('requests', function() {
-    it('should respond with a 403 status code when a POST to "/" is made without a CSRF token', function(done) {
+  describe('requests', function () {
+    it('should respond with a 403 status code when a POST to "/" is made without a CSRF token', function (done) {
       $http.post('/', (err, res) => {
         expect(err).to.be.null;
 
@@ -147,7 +203,7 @@ describe('Fi Security', function() {
       });
     });
 
-    it('should respond with a 204 status code when a POST to "/" is made with a CSRF token as form data', function(done) {
+    it('should respond with a 204 status code when a POST to "/" is made with a CSRF token as form data', function (done) {
       $http.post(
         {
           uri: '/',
@@ -166,7 +222,7 @@ describe('Fi Security', function() {
       );
     });
 
-    it('should respond with a 204 status code when a POST to "/" is made with a CSRF token as header param', function(done) {
+    it('should respond with a 204 status code when a POST to "/" is made with a CSRF token as header param', function (done) {
       $http.post(
         {
           uri: '/',
@@ -185,7 +241,7 @@ describe('Fi Security', function() {
       );
     });
 
-    it('should respond with a 204 status code when a POST to "/no-csrf" is made without a CSRF token', function(done) {
+    it('should respond with a 204 status code when a POST to "/no-csrf" is made without a CSRF token', function (done) {
       $http.post('/no-csrf', (err, res) => {
         expect(err).to.be.null;
 
